f39a7bec1b7c9ba1c206444b0999328b8153f2d1
Queenie Li <queenie_li_@hotmail.com>
2018-07-08 15:50:29 -0400

    update navbar css

--- app/pages/home/HomePageContainer.jsx
+++ app/pages/home/HomePageContainer.jsx
@@ -1,11 +1,112 @@
 import React, {Component} from 'react'
 
+
 export default class HomePageContainer extends Component {
 
 	render() {
 		return (
-			<div>
+			<div className="homecontainerTest">
 				Welcome to Passerby Coffee!
+                Working with a Partner
+                You have the option of partnering with one other CSC343 student for your assignments, and we encourage you to do so. You may choose your own partner, and it need not be the same person for each assignment. Once you begin working on an assignment, you may not dissolve your partnership without my permission. Both partners will receive the same mark for joint assignments. Working with a partner has the potential to lighten your workload and enhance your learning or to increase your workload and impair your learning, depending on how you work together. Remember that you are responsible for learning the course material underlying all parts of the assignments. You will have the most success if you truly work together.
+
+                Assignment Policies
+                Assignments must be submitted electronically, using the MarkUs online system. Log in with your CS Teaching Labs username and password. Be sure to confirm that you have submitted all the required files and the correct version of each; we cannot accept missing files or a different version of an already-submitted file after the due date. Code that you submit to us for grading must work on the CS Teaching Labs machines in order to earn credit.
+
+                Late Assignments
+                We recognize that unexpected problems sometimes make it difficult to submit assignments on time. For this reason, we will be using grace tokens to give you flexibility with assignment deadlines.
+
+                Each student will receive six grace tokens; each grace token can be used for a two-hour extension for an assignment. For example, you may choose to use all six grace tokens on the first assignment, extending its deadline by twelve hours . Or, you may wish to use two tokens for each assignment, extending each deadline by four hours. If working in group then 2 hour extension consumes one token from each partner.
+
+                MarkUs automatically deducts grace tokens when you submit an assignment late - you do not need to explicitly say you are using a grace token. If you work with a partner on an assignment, grace tokens are deducted from every team member, not just one of you.
+
+                You may not use grace tokens to extend the deadline of the weekly homeworks.
+                Special Consideration
+                If you are unable to complete homework or if you miss a test due to major illness or other circumstances completely outside of your control, get in touch with us immediately if you want to receive special consideration.
+
+                In order to receive special consideration, you must fill out a Request for Special Consideration Form. Bring the form to one of us right away, together with your supporting documentation. In the case of illness, please have a doctor complete a Verification of Student Illness or Injury Form. For other emergencies, be prepared to provide some kind of documentation.
+
+                Remark Requests
+                If you feel there was an error in the marking of an assignment or test, you may request that it be remarked on MarkUs. You must give a specific reason for the request, referring to a possible error or omission by the marker.
+
+                For prompt turnaround, remark requests must be received within one week of when the item was returned.
+
+                Academic Integrity
+                The work you submit must be your own. It is an academic offence to copy someone else's work. This includes their code, their words, and even their ideas. Whether you copy or let someone else copy, it is an offence. Academic offences are taken very seriously.
+
+                At the same time, we want you to benefit from working with other students. Obviously, work done with your partner is a joint effort. You are also welcome to work appropriately with students other than your partner. It is appropriate to discuss course material and technology related to assignments, and we encourage you to do so. For example, you may work through examples that help you understand course material or a new technology, or help each other configure your system to run a supporting piece of software. You may also discuss assignment requirements.
+
+                However, other than between partners, collaboration on assignment solutions is strictly forbidden. The most certain way to protect yourself is not to discuss assignment solutions or the ideas behind them with students other than your partner. Certainly you must not let others see your assignment solutions, even in draft form. Please don't cheat. We want you to succeed and are here to help if you are having difficulty.
+
+                Accessibility Needs
+                The University of Toronto is committed to accessibility. If you require accommodations or have any accessibility concerns, please visit Accessibility Services as soon as possible.
+                Teaching team
+                Dr. Tamanna Chhabra: email
+                Safoura Janosepah: email
+                Fatemeh Nargesian: email
+                Matthew Wong: email
+                Zannis Xarchakos: email
+                Locations
+                Lectures: Thursday 6:00 - 9:00 PM, MS 2172
+                Office hours: Thursday 4:00 - 5:30 PM, BA3201
+                Feedback
+
+                Your name (optional)
+
+                Your email (optional)
+
+                Tell us...
+                Send
+                Working with a Partner
+                You have the option of partnering with one other CSC343 student for your assignments, and we encourage you to do so. You may choose your own partner, and it need not be the same person for each assignment. Once you begin working on an assignment, you may not dissolve your partnership without my permission. Both partners will receive the same mark for joint assignments. Working with a partner has the potential to lighten your workload and enhance your learning or to increase your workload and impair your learning, depending on how you work together. Remember that you are responsible for learning the course material underlying all parts of the assignments. You will have the most success if you truly work together.
+
+                Assignment Policies
+                Assignments must be submitted electronically, using the MarkUs online system. Log in with your CS Teaching Labs username and password. Be sure to confirm that you have submitted all the required files and the correct version of each; we cannot accept missing files or a different version of an already-submitted file after the due date. Code that you submit to us for grading must work on the CS Teaching Labs machines in order to earn credit.
+
+                Late Assignments
+                We recognize that unexpected problems sometimes make it difficult to submit assignments on time. For this reason, we will be using grace tokens to give you flexibility with assignment deadlines.
+
+                Each student will receive six grace tokens; each grace token can be used for a two-hour extension for an assignment. For example, you may choose to use all six grace tokens on the first assignment, extending its deadline by twelve hours . Or, you may wish to use two tokens for each assignment, extending each deadline by four hours. If working in group then 2 hour extension consumes one token from each partner.
+
+                MarkUs automatically deducts grace tokens when you submit an assignment late - you do not need to explicitly say you are using a grace token. If you work with a partner on an assignment, grace tokens are deducted from every team member, not just one of you.
+
+                You may not use grace tokens to extend the deadline of the weekly homeworks.
+                Special Consideration
+                If you are unable to complete homework or if you miss a test due to major illness or other circumstances completely outside of your control, get in touch with us immediately if you want to receive special consideration.
+
+                In order to receive special consideration, you must fill out a Request for Special Consideration Form. Bring the form to one of us right away, together with your supporting documentation. In the case of illness, please have a doctor complete a Verification of Student Illness or Injury Form. For other emergencies, be prepared to provide some kind of documentation.
+
+                Remark Requests
+                If you feel there was an error in the marking of an assignment or test, you may request that it be remarked on MarkUs. You must give a specific reason for the request, referring to a possible error or omission by the marker.
+
+                For prompt turnaround, remark requests must be received within one week of when the item was returned.
+
+                Academic Integrity
+                The work you submit must be your own. It is an academic offence to copy someone else's work. This includes their code, their words, and even their ideas. Whether you copy or let someone else copy, it is an offence. Academic offences are taken very seriously.
+
+                At the same time, we want you to benefit from working with other students. Obviously, work done with your partner is a joint effort. You are also welcome to work appropriately with students other than your partner. It is appropriate to discuss course material and technology related to assignments, and we encourage you to do so. For example, you may work through examples that help you understand course material or a new technology, or help each other configure your system to run a supporting piece of software. You may also discuss assignment requirements.
+
+                However, other than between partners, collaboration on assignment solutions is strictly forbidden. The most certain way to protect yourself is not to discuss assignment solutions or the ideas behind them with students other than your partner. Certainly you must not let others see your assignment solutions, even in draft form. Please don't cheat. We want you to succeed and are here to help if you are having difficulty.
+
+                Accessibility Needs
+                The University of Toronto is committed to accessibility. If you require accommodations or have any accessibility concerns, please visit Accessibility Services as soon as possible.
+                Teaching team
+                Dr. Tamanna Chhabra: email
+                Safoura Janosepah: email
+                Fatemeh Nargesian: email
+                Matthew Wong: email
+                Zannis Xarchakos: email
+                Locations
+                Lectures: Thursday 6:00 - 9:00 PM, MS 2172
+                Office hours: Thursday 4:00 - 5:30 PM, BA3201
+                Feedback
+
+                Your name (optional)
+
+                Your email (optional)
+
+                Tell us...
+                Send
 			</div>
 		)
 	}
