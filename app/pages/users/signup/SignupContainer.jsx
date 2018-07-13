43a67bdbeaaf56310fb916a7e7351207243ce6db
Queenie Li <queenie_li_@hotmail.com>
2018-07-12 22:52:23 -0400

    fixed css name

--- app/pages/users/signup/SignupContainer.jsx
+++ app/pages/users/signup/SignupContainer.jsx
@@ -4,27 +4,27 @@ import SignupPage from './SignupPage'
 import {signup} from "../../../actions/userActions"
 
 class SignupContainer extends Component {
-	render() {
-		return (
-			<div className="body">
-				<SignupPage
-					userSignupState={this.props.userSignupState}
-					signupHandler={this.signupHandler.bind(this)}
-				/>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div>
+                <SignupPage
+                    userSignupState={this.props.userSignupState}
+                    signupHandler={this.signupHandler.bind(this)}
+                />
+            </div>
+        )
+    }
 
-	signupHandler(firstName, lastName, preferredName, phone, email, password, consented) {
-		this.props.dispatch(signup(firstName, lastName, preferredName, phone, email, password, consented))
-	}
+    signupHandler(firstName, lastName, preferredName, phone, email, password, consented) {
+        this.props.dispatch(signup(firstName, lastName, preferredName, phone, email, password, consented))
+    }
 
 }
 
 function select(state) {
-	return {
-		userSignupState: state.userSignupState
-	}
+    return {
+        userSignupState: state.userSignupState
+    }
 }
 
 export default connect(select)(SignupContainer)
