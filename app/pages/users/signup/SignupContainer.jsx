9b81150881193081b21cb4b6cccc38b5feb3a567
Queenie Li <queenie_li_@hotmail.com>
2018-07-28 16:34:50 -0400

    add components

--- app/pages/users/signup/SignupContainer.jsx
+++ app/pages/users/signup/SignupContainer.jsx
@@ -1,12 +1,20 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import SignupPage from './SignupPage'
-import {signup} from "../../../actions/userActions"
+import {signup} from '../../../actions/userActions'
+import SignupPageTitle from './SignupPageTitle'
+import SignupPageTitleBar from './SignupPageTitleBar'
+import SignupPageProgressBar from "./SignupPageProgressBar"
 
 class SignupContainer extends Component {
     render() {
         return (
             <div>
+                <div className="title-container">
+                    <SignupPageTitle />
+                    <SignupPageTitleBar />
+                    <SignupPageProgressBar/>
+                </div>
                 <SignupPage
                     userSignupState={this.props.userSignupState}
                     signupHandler={this.signupHandler.bind(this)}
