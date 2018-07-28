75572b85078a9b611091a224bc7df3d1b1207bd8
Queenie Li <queenie_li_@hotmail.com>
2018-07-28 16:30:35 -0400

    Add components

---
+++ app/pages/users/signup/SignupPageTitleBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SignupPageTitleBar extends Component {
+    render() {
+        return (
+            <div>
+                <hr className="line-separator" />
+                <hr className="bold-line-separator" />
+            </div>
+        )
+    }
+}
